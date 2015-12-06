972929c5988d04e2fa6933e28b6b37c6e874849e
Kaburagi <kaburt@jpxxkaburtm2.corp.emc.com>
2015-12-06 12:24:26 +0900

    delete target

--- demo-onlinestore-order/target/classes/import.sql
+++
@@ -1,3 +0,0 @@
-insert into orderhistory (product, username) values ('Good Desk', 'tkaburagi');
-insert into orderhistory (product, username) values ('iPhone 3GS', 'robmee');
-insert into orderhistory (product, username) values ('iPhone 6', 'testuser');
